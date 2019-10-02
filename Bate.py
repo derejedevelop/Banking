699c4d156fc9324a59907d78eea6b25757b0fe3c
derejedevelop <dmdessalegne@gmail.com>
2019-10-02 02:03:16 -0400

    Some lines added

--- Bate.py
+++ Bate.py
@@ -1,1 +1,11 @@
 print("This is game is called banking.")
+
+print("----------------")
+print("|              |")
+print("|              |")
+print("|    BANK      |")
+print("|              |")
+print("|              |")
+print("----------------")
+
+
